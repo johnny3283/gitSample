61e746cce479d64745b604496c671966231e60db
johnny3283 <34590616+johnny3283@users.noreply.github.com>
2022-11-05 14:52:53 +0800

    3nd version

--- gitSample/src/gitSample/MainApp.java
+++ gitSample/src/gitSample/MainApp.java
@@ -1,7 +1,16 @@
 package gitSample;
 
 public class MainApp {
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		System.out.println("2nd Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(5, 5));		
+	}
+}
+
+class Math {
+	int add(int x, int y) {
+		return x + y;
 	}
 }
